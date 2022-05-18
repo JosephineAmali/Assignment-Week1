3fc7df67c7cb427c73393eca8a5280f541a52d08
JosephineAmali <103916257+JosephineAmali@users.noreply.github.com>
2022-05-18 14:21:18 +0100

    Add files via upload

---
+++ MissingElementInArray.java
@@ -0,0 +1,24 @@
+package Assignment1;
+
+import java.util.Arrays;
+
+public class MissingElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,4,5,7};
+		int j;
+				
+		Arrays.sort(arr);
+		for(int i =0; i <= arr.length -1; i++)
+		{
+			if(i+1 != arr[i])
+			{
+				System.out.println(i+1);
+				break;
+			}
+		}
+		
+	}
+
+}
